28d19d88381ce5083a48ab75ff47710e5590ffea
Priyanshi Negi <121029180+PriyanshiNegi01@users.noreply.github.com>
2023-11-16 14:12:50 +0530

    Update server7.c

--- 07-Full Duplex Chat Using TCP-IP/server7.c
+++ 07-Full Duplex Chat Using TCP-IP/server7.c
@@ -14,26 +14,27 @@ int main(int argc,char *argv[])
     struct sockaddr_in serverAddress,clientAddress;
     socklen_t clientLength;
 
-    char recvBuffer[1000],sendBuffer[10000];
+    char recvBuffer[1000],sendBuffer[1000];
     pid_t cpid;
     bzero(&serverAddress,sizeof(serverAddress));
-    /*Socket address structure*/
+    
+    // Socket address structure
     serverAddress.sin_family=AF_INET;
     serverAddress.sin_addr.s_addr=htonl(INADDR_ANY);
     serverAddress.sin_port=htons(5500);
-    /*TCP socket is created, an Internet socket address structure is filled with
-    wildcard address & server’s well known port*/
+    
+    // Socket creation
     socketDescriptor=socket(AF_INET,SOCK_STREAM,0);
     /*Bind function assigns a local protocol address to the socket*/
     bind(socketDescriptor,(struct sockaddr*)&serverAddress,sizeof(serverAddress));
-    /*Listen function specifies the maximum number of connections that kernel should queue
-    for this socket*/
+    //Listen function specifies the maximum number of connections that can be accepted
     listen(socketDescriptor,5);
     printf("%s\n","Server is running ...");
+    
     /*The server to return the next completed connection from the front of the
     completed connection Queue calls it*/
     clientSocketDescriptor=accept(socketDescriptor,(struct sockaddr*)&clientAddress,&clientLength);
-    /*Fork system call is used to create a new process*/
+    //Fork system call is used to create a new process
     cpid=fork();
     
     if(cpid==0)
@@ -41,9 +42,9 @@ int main(int argc,char *argv[])
         while(1)
         {
             bzero(&recvBuffer,sizeof(recvBuffer));
-            /*Receiving the request from client*/
+            //Receiving the request from client
             recv(clientSocketDescriptor,recvBuffer,sizeof(recvBuffer),0);
-            printf("\nCLIENT : %s\n",recvBuffer);
+            printf("\nCLIENT: %s\n",recvBuffer);
         }
     }
     else
@@ -51,12 +52,12 @@ int main(int argc,char *argv[])
         while(1)
         {
             bzero(&sendBuffer,sizeof(sendBuffer));
-            printf("\nType a message here ...  ");
-            /*Read the message from client*/
-            fgets(sendBuffer,10000,stdin);
-            /*Sends the message to client*/
+            printf("\nEnter a message: ");
+            //Read the message from client
+            fgets(sendBuffer,1000,stdin);
+            //Sends the message to client
             send(clientSocketDescriptor,sendBuffer,strlen(sendBuffer)+1,0);
-            printf("\nMessage sent !\n");
+            printf("\nMessage sent!\n");
         }
     }
     return 0;
