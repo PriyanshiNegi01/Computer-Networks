233862b1d0f43617284c44efaef6fb365ed4557b
Priyanshi Negi <121029180+PriyanshiNegi01@users.noreply.github.com>
2023-11-16 14:13:57 +0530

    Update client7.c

--- 07-Full Duplex Chat Using TCP-IP/client7.c
+++ 07-Full Duplex Chat Using TCP-IP/client7.c
@@ -14,37 +14,37 @@
 int main()
 {
     int socketDescriptor;
-
+    
     struct sockaddr_in serverAddress;
-    char sendBuffer[10000],recvBuffer[1000];
-
+    char sendBuffer[1000],recvBuffer[1000];
+    
     pid_t cpid;
-
+    
     bzero(&serverAddress,sizeof(serverAddress));
-
+    
     serverAddress.sin_family=AF_INET;
     serverAddress.sin_addr.s_addr=inet_addr("127.0.0.1");
     serverAddress.sin_port=htons(5500);
-
-    /*Creating a socket, assigning IP address and port number for that socket*/
+    
+    //Socket creation, assigning IP address and port number for that socket
     socketDescriptor=socket(AF_INET,SOCK_STREAM,0);
     
-    /*Connect establishes connection with the server using server IP address*/
+    //Connection establishment with the server using server IP address
     connect(socketDescriptor,(struct sockaddr*)&serverAddress,sizeof(serverAddress));
     
-    /*Fork is used to create a new process*/
+    //Fork is used to create a new process
     cpid=fork();
     if(cpid==0)
     {
         while(1)
         {
             bzero(&sendBuffer,sizeof(sendBuffer));
-            printf("\nType a message here ...  ");
-            /*This function is used to read from server*/
-            fgets(sendBuffer,10000,stdin);
-            /*Send the message to server*/
+            printf("\nEnter a message: ");
+            //This function is used to read from server
+            fgets(sendBuffer,1000,stdin);
+            //Send the message to server
             send(socketDescriptor,sendBuffer,strlen(sendBuffer)+1,0);
-            printf("\nMessage sent !\n");
+            printf("\nMessage sent!\n");
         }
     }
     else
@@ -52,9 +52,9 @@ int main()
         while(1)
         {
             bzero(&recvBuffer,sizeof(recvBuffer));
-            /*Receive the message from server*/
+            //Receive the message from server
             recv(socketDescriptor,recvBuffer,sizeof(recvBuffer),0);
-            printf("\nSERVER : %s\n",recvBuffer);
+            printf("\nSERVER: %s\n",recvBuffer);
         }
     }
     return 0;
